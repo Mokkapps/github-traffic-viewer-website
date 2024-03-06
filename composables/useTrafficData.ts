93f6fd7e07ce427b7846299074250b01cfcf889c
Michael Hoffmann <michael.hoffmann@mokkapps.de>
2024-03-06 19:41:20 +0100

    refactor: add useTrafficData composable

---
+++ composables/useTrafficData.ts
@@ -0,0 +1,222 @@
+export const useTrafficData = () => {
+  const { githubAccessToken, githubUserName } = useAuth()
+  const route = useRoute()
+  const router = useRouter()
+
+  const showForkedRepos = ref(false)
+  const showPrivateRepos = ref(false)
+  const trafficTimeFrame = ref<'day' | 'week'>('week')
+  const selectedRepositoryId = ref<number | null>(null)
+  const searchRepositoryName = ref('')
+
+  const { data, error, status } = useAsyncData(
+    'traffic-data',
+    async () => {
+      if (!githubUserName.value) {
+        return
+      }
+
+      if (!githubAccessToken.value) {
+        throw new Error('No GitHub access token found. Please try to login again.')
+      }
+
+      const headers = {
+        Accept: 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28',
+        Authorization: `Bearer ${githubAccessToken.value}`,
+      }
+
+      const REPOS_PER_PAGE = 100
+      const NEXT_PATTERN = /(?<=<)([\S]*)(?=>; rel="Next")/i
+      let pagesRemaining = true
+      let repos: Array<GithubRepositoryDTO> = []
+
+      // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-the-authenticated-user
+      let url = `https://api.github.com/user/repos?per_page=${REPOS_PER_PAGE}&page=1`
+
+      while (pagesRemaining) {
+        console.log('fetching repos', url)
+        const reposResponse = await $fetch.raw(url, { headers })
+
+        repos = [...repos, ...(reposResponse._data as Array<GithubRepositoryDTO>)]
+
+        const linkHeader = reposResponse.headers.get('link')
+
+        pagesRemaining = linkHeader && linkHeader.includes('rel="next"')
+
+        if (pagesRemaining) {
+          url = linkHeader.match(NEXT_PATTERN)[0]
+        }
+      }
+
+      const mappedRepos: Array<Pick<RepositoryViewModel, 'id' | 'isFork' | 'isPrivate' | 'name'>> = repos.map(
+        (repo) => ({
+          name: repo.name,
+          isFork: repo.fork,
+          isPrivate: repo.private,
+          id: repo.id,
+        }),
+      )
+
+      const results = await Promise.allSettled(
+        mappedRepos.map(async (mappedRepo) => {
+          // https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28#get-page-views
+          const trafficData = await $fetch<TrafficData>(
+            `https://api.github.com/repos/${githubUserName.value}/${mappedRepo.name}/traffic/views?per=${trafficTimeFrame.value}`,
+            {
+              headers,
+            },
+          )
+          return {
+            ...mappedRepo,
+            trafficData,
+          }
+        }),
+      )
+
+      return results
+        .filter((result) => result.status === 'fulfilled')
+        .map((result) => result.value as RepositoryViewModel)
+    },
+    { watch: [githubUserName, githubAccessToken, trafficTimeFrame] },
+  )
+
+  const isLoadingTrafficData = computed(() => status.value === 'pending')
+
+  const headerText = computed(() => `Traffic data for your GitHub user "${githubUserName.value}"`)
+
+  const selectedRepositoryData = computed(() => {
+    if (!filteredRepositoriesData.value) {
+      return undefined
+    }
+
+    return filteredRepositoriesData.value.find((repo) => repo.id === selectedRepositoryId.value)
+  })
+
+  const filteredRepositoriesData = computed(() => {
+    if (!data.value) {
+      return []
+    }
+
+    return data.value.filter((repo) => {
+      if (
+        searchRepositoryName.value !== '' &&
+        !repo.name.toLowerCase().includes(searchRepositoryName.value.toLowerCase())
+      ) {
+        return false
+      }
+
+      if (!showForkedRepos.value && repo.isFork) {
+        return false
+      }
+
+      if (!showPrivateRepos.value && repo.isPrivate) {
+        return false
+      }
+
+      return true
+    })
+  })
+
+  watch(data, () => {
+    if (data.value && !selectedRepositoryId.value) {
+      selectedRepositoryId.value = data.value[0].id
+    }
+  })
+
+  watch(
+    selectedRepositoryId,
+    (newSelectedRepositoryId) => {
+      router.push({
+        query: {
+          ...route.query,
+          selectedRepositoryId: newSelectedRepositoryId,
+        },
+      })
+    },
+    { immediate: true },
+  )
+
+  watch(
+    showForkedRepos,
+    (newShowForkedRepos) => {
+      router.push({
+        query: {
+          ...route.query,
+          showForkedRepos: newShowForkedRepos ? 'true' : undefined,
+        },
+      })
+    },
+    { immediate: true },
+  )
+
+  watch(
+    showPrivateRepos,
+    (newShowPrivateRepos) => {
+      router.push({
+        query: {
+          ...route.query,
+          showPrivateRepos: newShowPrivateRepos ? 'true' : undefined,
+        },
+      })
+    },
+    { immediate: true },
+  )
+
+  watch(
+    searchRepositoryName,
+    (newSearchName) => {
+      router.push({
+        query: {
+          ...route.query,
+          q: newSearchName || '',
+        },
+      })
+    },
+    { immediate: true },
+  )
+
+  watch(
+    trafficTimeFrame,
+    (newTrafficTimeFrame) => {
+      router.push({
+        query: {
+          ...route.query,
+          trafficTimeFrame: newTrafficTimeFrame,
+        },
+      })
+    },
+    { immediate: true },
+  )
+
+  onBeforeMount(() => {
+    if (route.query.selectedRepositoryId) {
+      selectedRepositoryId.value = Number(route.query.selectedRepositoryId)
+    }
+    if (route.query.showForkedRepos) {
+      showForkedRepos.value = route.query.showForkedRepos === 'true'
+    }
+    if (route.query.showPrivateRepos) {
+      showPrivateRepos.value = route.query.showPrivateRepos === 'true'
+    }
+    if (route.query.trafficTimeFrame) {
+      trafficTimeFrame.value = route.query.trafficTimeFrame as 'day' | 'week'
+    }
+    if (route.query.q) {
+      searchRepositoryName.value = route.query.q as string
+    }
+  })
+
+  return {
+    showForkedRepos,
+    showPrivateRepos,
+    searchRepositoryName,
+    headerText,
+    trafficTimeFrame,
+    selectedRepositoryData,
+    repositories: filteredRepositoriesData,
+    isLoadingTrafficData,
+    error,
+    selectedRepositoryId,
+  }
+}
--- pages/traffic-data.vue
+++ pages/traffic-data.vue
@@ -1,13 +1,16 @@
 <script lang="ts" setup>
-const { githubAccessToken, githubUserName } = useAuth()
-const route = useRoute()
-const router = useRouter()
-
-const showForkedRepos = ref(false)
-const showPrivateRepos = ref(false)
-const trafficTimeFrame = ref<'day' | 'week'>('week')
-const selectedRepositoryId = ref<number | null>(null)
-const searchRepositoryName = ref('')
+const {
+  showForkedRepos,
+  showPrivateRepos,
+  searchRepositoryName,
+  headerText,
+  selectedRepositoryData,
+  repositories,
+  trafficTimeFrame,
+  isLoadingTrafficData,
+  error,
+  selectedRepositoryId,
+} = useTrafficData()
 
 const links = [
   {
@@ -21,207 +24,6 @@ const links = [
     },
   },
 ]
-
-const headerText = computed(() => `Traffic data for your GitHub user "${githubUserName.value}"`)
-
-const selectedRepositoryData = computed(() => {
-  if (!filteredRepositoriesData.value) {
-    return undefined
-  }
-
-  return filteredRepositoriesData.value.find((repo) => repo.id === selectedRepositoryId.value)
-})
-
-const filteredRepositoriesData = computed(() => {
-  if (!reposData.value) {
-    return []
-  }
-
-  return reposData.value.filter((repo) => {
-    if (
-      searchRepositoryName.value !== '' &&
-      !repo.name.toLowerCase().includes(searchRepositoryName.value.toLowerCase())
-    ) {
-      return false
-    }
-
-    if (!showForkedRepos.value && repo.isFork) {
-      return false
-    }
-
-    if (!showPrivateRepos.value && repo.isPrivate) {
-      return false
-    }
-
-    return true
-  })
-})
-
-const {
-  data: reposData,
-  error: reposError,
-  pending: isLoadingTrafficData,
-} = useAsyncData(
-  'traffic-data',
-  async () => {
-    if (!githubUserName.value) {
-      return
-    }
-
-    if (!githubAccessToken.value) {
-      throw new Error('No GitHub access token found.')
-    }
-
-    const headers = {
-      Accept: 'application/vnd.github+json',
-      'X-GitHub-Api-Version': '2022-11-28',
-      Authorization: `Bearer ${githubAccessToken.value}`,
-    }
-
-    const REPOS_PER_PAGE = 100
-    const NEXT_PATTERN = /(?<=<)([\S]*)(?=>; rel="Next")/i
-    let pagesRemaining = true
-    let repos: Array<GithubRepositoryDTO> = []
-
-    // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-the-authenticated-user
-    let url = `https://api.github.com/user/repos?per_page=${REPOS_PER_PAGE}&page=1`
-
-    while (pagesRemaining) {
-      console.log('fetching repos', url)
-      const reposResponse = await $fetch.raw(url, { headers })
-
-      repos = [...repos, ...(reposResponse._data as Array<GithubRepositoryDTO>)]
-
-      const linkHeader = reposResponse.headers.get('link')
-
-      pagesRemaining = linkHeader && linkHeader.includes('rel="next"')
-
-      if (pagesRemaining) {
-        url = linkHeader.match(NEXT_PATTERN)[0]
-      }
-    }
-
-    console.log(repos)
-    // console.log('link header', reposResponse.headers.get('link'))
-
-    const mappedRepos: Array<Pick<RepositoryViewModel, 'id' | 'isFork' | 'isPrivate' | 'name'>> = repos.map((repo) => ({
-      name: repo.name,
-      isFork: repo.fork,
-      isPrivate: repo.private,
-      id: repo.id,
-    }))
-
-    const results = await Promise.allSettled(
-      mappedRepos.map(async (mappedRepo) => {
-        // https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28#get-page-views
-        const trafficData = await $fetch<TrafficData>(
-          `https://api.github.com/repos/${githubUserName.value}/${mappedRepo.name}/traffic/views?per=${trafficTimeFrame.value}`,
-          {
-            headers,
-          },
-        )
-        return {
-          ...mappedRepo,
-          trafficData,
-        }
-      }),
-    )
-
-    return results
-      .filter((result) => result.status === 'fulfilled')
-      .map((result) => result.value as GithubRepositoryDTO)
-  },
-  { watch: [githubUserName, githubAccessToken, trafficTimeFrame] },
-)
-
-watch(reposData, () => {
-  if (reposData.value && !selectedRepositoryId.value) {
-    selectedRepositoryId.value = reposData.value[0].id
-  }
-})
-
-watch(
-  selectedRepositoryId,
-  (newSelectedRepositoryId) => {
-    router.push({
-      query: {
-        ...route.query,
-        selectedRepositoryId: newSelectedRepositoryId,
-      },
-    })
-  },
-  { immediate: true },
-)
-
-watch(
-  showForkedRepos,
-  (newShowForkedRepos) => {
-    router.push({
-      query: {
-        ...route.query,
-        showForkedRepos: newShowForkedRepos ? 'true' : undefined,
-      },
-    })
-  },
-  { immediate: true },
-)
-
-watch(
-  showPrivateRepos,
-  (newShowPrivateRepos) => {
-    router.push({
-      query: {
-        ...route.query,
-        showPrivateRepos: newShowPrivateRepos ? 'true' : undefined,
-      },
-    })
-  },
-  { immediate: true },
-)
-
-watch(
-  searchRepositoryName,
-  (newSearchName) => {
-    router.push({
-      query: {
-        ...route.query,
-        q: newSearchName || '',
-      },
-    })
-  },
-  { immediate: true },
-)
-
-watch(
-  trafficTimeFrame,
-  (newTrafficTimeFrame) => {
-    router.push({
-      query: {
-        ...route.query,
-        trafficTimeFrame: newTrafficTimeFrame,
-      },
-    })
-  },
-  { immediate: true },
-)
-
-onBeforeMount(() => {
-  if (route.query.selectedRepositoryId) {
-    selectedRepositoryId.value = Number(route.query.selectedRepositoryId)
-  }
-  if (route.query.showForkedRepos) {
-    showForkedRepos.value = route.query.showForkedRepos === 'true'
-  }
-  if (route.query.showPrivateRepos) {
-    showPrivateRepos.value = route.query.showPrivateRepos === 'true'
-  }
-  if (route.query.trafficTimeFrame) {
-    trafficTimeFrame.value = route.query.trafficTimeFrame as 'day' | 'week'
-  }
-  if (route.query.q) {
-    searchRepositoryName.value = route.query.q as string
-  }
-})
 
 definePageMeta({
   layout: 'dashboard',
@@ -247,26 +49,15 @@ definePageMeta({
           v-model:name="searchRepositoryName"
           v-model:traffic-time-frame="trafficTimeFrame"
           :is-loading-traffic-data="isLoadingTrafficData"
-          :repositories="filteredRepositoriesData"
+          :repositories="repositories"
           @select="selectedRepositoryId = $event"
         />
-
-        <div class="flex-1" />
       </UDashboardSidebar>
     </UDashboardPanel>
 
     <UDashboardPage>
       <UDashboardPanel grow>
         <UDashboardNavbar :title="headerText">
-          <TrafficDataSidebar
-            v-model:show-forked-repos="showForkedRepos"
-            v-model:show-private-repos="showPrivateRepos"
-            v-model:name="searchRepositoryName"
-            v-model:traffic-time-frame="trafficTimeFrame"
-            :is-loading-traffic-data="isLoadingTrafficData"
-            :repositories="filteredRepositoriesData"
-            @select="selectedRepositoryId = $event"
-          />
           <template #right>
             <UColorModeButton />
 
@@ -283,21 +74,23 @@ definePageMeta({
 
         <UDashboardPanelContent>
           <div class="col-span-9 p-8">
-            <RepoTrafficChart v-if="selectedRepositoryData" :repository="selectedRepositoryData" />
-            <UCard v-else-if="isLoadingTrafficData" class="mt-10">
-              <template #header>
-                <USkeleton class="h-4 w-[250px]" />
-              </template>
-              <USkeleton class="h-4 w-[200px]" />
+            <UCard v-if="isLoadingTrafficData">
+              <div class="flex flex-col gap-10">
+                <USkeleton class="h-6 w-[300px]" />
+                <div class="flex flex-col gap-8">
+                  <USkeleton v-for="n of 13" :key="n" class="h-4 w-full" />
+                </div>
+              </div>
             </UCard>
+            <RepoTrafficChart v-else-if="selectedRepositoryData" :repository="selectedRepositoryData" />
             <UAlert
-              v-else-if="reposError"
+              v-else-if="error"
               class="mt-10"
               icon="i-heroicons-exclamation-circle"
               color="red"
               variant="outline"
               title="An error occurred"
-              :description="reposError.message"
+              :description="error.message"
             />
           </div>
         </UDashboardPanelContent>
